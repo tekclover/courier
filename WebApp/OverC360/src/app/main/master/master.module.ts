f08867739691faee42725bd2c9ff0820540ac040
Mugilan2298 <mugilan.k@tekclover.com>
2024-06-25 18:47:47 +0530

    code done

--- WebApp/OverC360/src/app/main/master/master.module.ts
+++ WebApp/OverC360/src/app/main/master/master.module.ts
@@ -3,10 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { MasterRoutingModule } from './master-routing.module';
 import { SharedModule } from '../../shared/shared.module';
+import { HubComponent } from './hub/hub.component';
+import { HubNewComponent } from './hub/hub-new/hub-new.component';
+import { DistrictMappingComponent } from './district-mapping/district-mapping.component';
+import { DistrictMappingNewComponent } from './district-mapping/district-mapping-new/district-mapping-new.component';
+import { ProvinceMappingComponent } from './province-mapping/province-mapping.component';
+import { ProvinceMappingNewComponent } from './province-mapping/province-mapping-new/province-mapping-new.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HubComponent,
+    HubNewComponent,
+    DistrictMappingComponent,
+    DistrictMappingNewComponent,
+    ProvinceMappingComponent,
+    ProvinceMappingNewComponent
+  ],
   imports: [
     CommonModule,
     MasterRoutingModule,
