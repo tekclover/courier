2bc67ddc8ee54c2313ad6990352d743b5ba9078e
KarthikVUK <kavink.v@tekclover.com>
2024-06-26 13:39:18 +0530

    codChngs

---
+++ WebApp/OverC360/src/app/main/id-masters/rate-parameter/rate-parameter.component.ts
@@ -0,0 +1,201 @@
+import { Component } from '@angular/core';
+import { RateParameterService } from './rate-parameter.service';
+import { DatePipe } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { MessageService } from 'primeng/api';
+import { CustomTableComponent } from '../../../common-dialog/custom-table/custom-table.component';
+import { DeleteComponent } from '../../../common-dialog/delete/delete.component';
+import { CommonServiceService } from '../../../common-service/common-service.service';
+import { PathNameService } from '../../../common-service/path-name.service';
+
+@Component({
+  selector: 'app-rate-parameter',
+  templateUrl: './rate-parameter.component.html',
+  styleUrl: './rate-parameter.component.scss'
+})
+export class RateParameterComponent {
+
+  rateParameterTable: any[] = [];
+  selectedRateParameter: any[] = [];
+  cols: any[] = [];
+  target: any[] = [];
+
+  constructor(
+    private messageService: MessageService,
+    private cs: CommonServiceService,
+    private router: Router,
+    private path: PathNameService,
+    private service: RateParameterService,
+    public dialog: MatDialog,
+    private datePipe: DatePipe,
+    private spin: NgxSpinnerService
+  ) { }
+
+  fullDate: any;
+  today: any;
+  ngOnInit() {
+    //to pass the breadcrumbs value to the main component
+    const dataToSend = ['Setup', 'Rate Parameter - List'];
+    this.path.setData(dataToSend);
+
+    this.callTableHeader();
+    this.initialCall();
+  }
+
+  callTableHeader() {
+    this.cols = [
+      { field: 'companyName', header: 'Company' },
+      { field: 'rateParameterId', header: 'Rate Parameter ID' },
+      { field: 'rateParameterDescription', header: 'Rate Parameter Name' },
+      { field: 'remark', header: 'Remarks' },
+      { field: 'statusDescription', header: 'Status' },
+      { field: 'createdBy', header: 'Created By' },
+      { field: 'createdOn', header: 'Created On', format: 'date' },
+    ];
+    this.target = [
+      { field: 'languageId', header: 'Language ID' },
+      { field: 'languageDescription', header: 'Language' },
+      { field: 'companyId', header: 'Company ID' },
+      { field: 'statusId', header: 'Status ID' },
+      { field: 'referenceField1', header: 'Reference Field 1' },
+      { field: 'referenceField2', header: 'Reference Field 2' },
+      { field: 'referenceField3', header: 'Reference Field 3' },
+      { field: 'referenceField4', header: 'Reference Field 4' },
+      { field: 'referenceField5', header: 'Reference Field 5' },
+      { field: 'referenceField6', header: 'Reference Field 6' },
+      { field: 'referenceField7', header: 'Reference Field 7' },
+      { field: 'referenceField8', header: 'Reference Field 8' },
+      { field: 'referenceField9', header: 'Reference Field 9' },
+      { field: 'referenceField10', header: 'Reference Field 10' },
+      { field: 'updatedBy', header: 'Updated By' },
+      { field: 'updatedOn', header: 'Updated On', format: 'date' },
+    ];
+  }
+
+  initialCall() {
+    this.spin.show();
+    this.service.search({}).subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.rateParameterTable = res;
+        this.spin.hide();
+      },
+      error: (err) => {
+        this.spin.hide();
+        this.cs.commonerrorNew(err);
+      },
+    });
+  }
+
+  onChange() {
+    const choosen = this.selectedRateParameter[this.selectedRateParameter.length - 1];
+    this.selectedRateParameter.length = 0;
+    this.selectedRateParameter.push(choosen);
+  }
+
+  customTable() {
+    const dialogRef = this.dialog.open(CustomTableComponent, {
+      disableClose: true,
+      width: '70%',
+      maxWidth: '80%',
+      position: { top: '6.5%', left: '30%' },
+      data: { target: this.cols, source: this.target },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.deleterecord(this.selectedRateParameter[0]);
+      }
+    });
+  }
+
+  openCrud(type: any = 'New', linedata: any = null): void {
+    if (this.selectedRateParameter.length === 0 && type != 'New') {
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Warning',
+        key: 'br',
+        detail: 'Kindly select any Row',
+      });
+    } else {
+      let paramdata = this.cs.encrypt({
+        line: linedata == null ? this.selectedRateParameter[0] : linedata,
+        pageflow: type,
+      });
+      this.router.navigate(['/main/idMaster/rateParameter-new/' + paramdata]);
+    }
+  }
+
+  deleteDialog() {
+    if (this.selectedRateParameter.length === 0) {
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Warning',
+        key: 'br',
+        detail: 'Kindly select any Row',
+      });
+      return;
+    }
+    const dialogRef = this.dialog.open(DeleteComponent, {
+      disableClose: true,
+      width: '70%',
+      maxWidth: '82%',
+      position: { top: '6.5%', left: '30%' },
+      data: {
+        line: this.selectedRateParameter,
+        module: 'Rate Parameter',
+        body: 'This action cannot be undone. All values associated with this field will be lost.',
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.deleterecord(this.selectedRateParameter[0]);
+      }
+    });
+  }
+
+  deleterecord(lines: any) {
+    this.spin.show();
+    this.service.Delete(lines.rateParameterId).subscribe({
+      next: (res) => {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Deleted',
+          key: 'br',
+          detail: lines.rateParameterId + ' Deleted successfully',
+        });
+        this.spin.hide();
+        this.initialCall();
+      },
+      error: (err) => {
+        this.cs.commonerrorNew(err);
+        this.spin.hide();
+      },
+    });
+  }
+
+  downloadExcel() {
+    const exportData = this.rateParameterTable.map((item) => {
+      const exportItem: any = {};
+      this.cols.forEach((col) => {
+        if (col.format == 'date') {
+          console.log(3);
+          exportItem[col.field] = this.datePipe.transform(
+            item[col.field],
+            'dd-MM-yyyy'
+          );
+        } else {
+          exportItem[col.field] = item[col.field];
+        }
+      });
+      return exportItem;
+    });
+
+    // Call ExcelService to export data to Excel
+    this.cs.exportAsExcel(exportData, 'Rate Parameter');
+  }
+
+}
