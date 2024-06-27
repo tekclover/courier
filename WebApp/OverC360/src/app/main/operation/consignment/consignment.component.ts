22be3ee02cf87ae3b3c16bcbd421b019ccd4ee7f
Mugilan2298 <mugilan.k@tekclover.com>
2024-06-27 17:06:01 +0530

    COD E

--- WebApp/OverC360/src/app/main/operation/consignment/consignment.component.ts
+++ WebApp/OverC360/src/app/main/operation/consignment/consignment.component.ts
@@ -126,20 +126,11 @@ export class ConsignmentComponent {
   }
 
   openCrud(type: any = 'New', linedata: any = null): void {
-    console.log(3)
     if (this.selectedConsignment.length === 0 && type != 'New') {
-      this.messageService.add({
-        severity: 'warn',
-        summary: 'Warning',
-        key: 'br',
-        detail: 'Kindly select any Row',
-      });
+      this.messageService.add({ severity: 'warn', summary: 'Warning', key: 'br', detail: 'Kindly select any Row' });
     } else {
-      let paramdata = this.cs.encrypt({
-        line: linedata == null ? this.selectedConsignment[0] : linedata,
-        pageflow: type,
-      });
-      this.router.navigate(['/main/idMaster/consignment-new/' + paramdata]);
+      let paramdata = this.cs.encrypt({ line: linedata == null ? this.selectedConsignment[0] : linedata, pageflow: type });
+      this.router.navigate(['/main/idMaster/city-new/' + paramdata]);
     }
   }
 
--- WebApp/OverC360/src/app/main/operation/operation-routing.module.ts
+++ WebApp/OverC360/src/app/main/operation/operation-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConsignmentComponent } from './consignment/consignment.component';
+import { ConsignmentNewComponent } from './consignment/consignment-new/consignment-new.component';
 
 const routes: Routes = [
   {path:'consignment',component: ConsignmentComponent, data: { title: 'Operation', module: 'Consignment' } },
+  {path:'consignment-new',component: ConsignmentNewComponent, data: { title: 'Operation', module: 'Consignment' } },
 
 ];
 
