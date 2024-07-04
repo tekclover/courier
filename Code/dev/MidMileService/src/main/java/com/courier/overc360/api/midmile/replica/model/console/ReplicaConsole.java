90674a39f11c5bebb928d38f0ebc5c104998b6ed
M-C-Bharathi <bharathi.m@tekclover.com>
2024-07-04 15:18:42 +0530

    consignment_update

--- Code/dev/MidMileService/src/main/java/com/courier/overc360/api/midmile/replica/model/console/ReplicaConsole.java
+++ Code/dev/MidMileService/src/main/java/com/courier/overc360/api/midmile/replica/model/console/ReplicaConsole.java
@@ -113,10 +113,10 @@ public class ReplicaConsole {
     @Column(name = "EVENT_TEXT", columnDefinition = "nvarchar(50)")
     private String eventText;
 
-    @Column(name = "EVENT_TIMESTAMP", columnDefinition = "nvarchar(50)")
+    @Column(name = "EVENT_TIMESTAMP")
     private Date eventTimestamp;
 
-    @Column(name = "STATUS_TIMESTAMP", columnDefinition = "nvarchar(50)")
+    @Column(name = "STATUS_TIMESTAMP")
     private Date statusTimestamp;
 
     @Column(name = "SHIPMENT_BAG_ID")
