615afe18fab50139a4d617698dee2732af814da2
karthikeyan7297 <karthikeyan.ms@tekclover.com>
2024-07-06 19:57:10 +0300

    changes

--- WebApp/OverC360/src/app/main/airport/ccr/ccr.component.ts
+++ WebApp/OverC360/src/app/main/airport/ccr/ccr.component.ts
@@ -51,8 +51,8 @@ export class CcrComponent {
       { field: 'partnerHouseAirwayBill', header: 'Partner HAWB' },
       { field: 'description', header: 'Commodity' },
       { field: 'hsCode', header: 'HS Code' },
-      { field: 'eventText', header: 'Event' },
       { field: 'statusDescription', header: 'Status' },
+      { field: 'eventText', header: 'Event' },
       { field: 'createdBy', header: 'Created By' },
       { field: 'createdOn', header: 'Created On', format: 'date' },
     ];
