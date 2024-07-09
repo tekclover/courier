6fad950379eb2206cf603e754023a3844b51cfcb
karthikeyan7297 <karthikeyan.ms@tekclover.com>
2024-07-09 13:55:42 +0300

    changes

--- WebApp/OverC360/src/app/main/airport/console/console.component.ts
+++ WebApp/OverC360/src/app/main/airport/console/console.component.ts
@@ -50,7 +50,7 @@ export class ConsoleComponent {
       {  field: 'houseAirwayBill', header: 'Consignment No' },
       { field: 'partnerMasterAirwayBill', header: 'Partner MAWB' },
       { field: 'partnerHouseAirwayBill', header: 'Partner HAWB' },
-      { field: 'statusId', header: 'Status' },
+      { field: 'statusText', header: 'Status' },
       { field: 'eventText', header: 'Event' }, 
       { field: 'createdBy', header: 'Created By' },
       { field: 'createdOn', header: 'Created On', format: 'date' },
