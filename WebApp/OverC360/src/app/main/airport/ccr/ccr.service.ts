8fb5dc33526f14afbf05eb59f995da833b53cbac
karthikeyan7297 <karthikeyan.ms@tekclover.com>
2024-07-09 13:16:33 +0300

    changes

--- WebApp/OverC360/src/app/main/airport/ccr/ccr.service.ts
+++ WebApp/OverC360/src/app/main/airport/ccr/ccr.service.ts
@@ -31,10 +31,22 @@ export class CcrService {
   search(obj: any) {
     return this.http.post<any>('/overc-midmile-service/ccr/findCcr', obj);
   }
+  genearateLabel(obj: any) {
+    return this.http.post<any>('/overc-midmile-service/pdf/Label', obj);
+  }
+  genearateInvoice(obj: any) {
+    return this.http.post<any>('/overc-midmile-service/consignment/findConsignmentInvoice', obj);
+  }
+  
 
   uploadBayan(file: File, filePath:any) {
     const formData = new FormData();
     formData.append('file', file);
     return this.http.post<any>('/pdf/extract' + '?filePath=' + filePath, formData);
+  }
+  public uploadfile(file: File, location:any) {
+    let formParams = new FormData();
+    formParams.append('file', file)
+    return this.http.post('/doc-storage/multiUpload?' + `location=${location}`, formParams)
   }
 }
