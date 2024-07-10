789f208774caa4ec5f2cf090a83b67843e70ddcd
M-C-Bharathi <bharathi.m@tekclover.com>
2024-07-10 19:29:10 +0530

    login_cross_orgin

--- Code/dev/IDMasterService/src/main/java/com/courier/overc360/api/idmaster/controller/LoginController.java
+++ Code/dev/IDMasterService/src/main/java/com/courier/overc360/api/idmaster/controller/LoginController.java
@@ -11,12 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @Slf4j
+@CrossOrigin(origins = "*")
 @Validated
 @Api(tags = {"Login"}, value = "Login Operations related to User Login") // label for swagger
 @SwaggerDefinition(tags = {@Tag(name = "Login", description = "Operations related to Login")})
