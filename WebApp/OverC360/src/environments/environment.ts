ca51891adb337be1a28a3c11671a3dcd3a61fbe0
Mugilan2298 <mugilan.k@tekclover.com>
2024-07-25 13:30:24 +0530

    overc_dev_9.1

--- WebApp/OverC360/src/environments/environment.ts
+++ WebApp/OverC360/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     title: 'OVERC360',
-    apiURL: 'https://overc360.com/d-overc',
+    apiURL: 'http://3.110.159.76:9296/d-overc',
     OAUTH_CLIENT: 'pixeltrice',
     OAUTH_SECRET: 'pixeltrice-secret-key',
     name: 'development',
