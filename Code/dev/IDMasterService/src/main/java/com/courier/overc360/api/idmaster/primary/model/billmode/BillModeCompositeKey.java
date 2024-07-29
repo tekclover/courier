b801e0076209b2e684b68d842ac81258df407f36
M-C-Bharathi <bharathi.m@tekclover.com>
2024-07-29 11:18:20 +0530

    appUser_billMode

--- Code/dev/IDMasterService/src/main/java/com/courier/overc360/api/idmaster/primary/model/billmode/BillModeCompositeKey.java
+++ Code/dev/IDMasterService/src/main/java/com/courier/overc360/api/idmaster/primary/model/billmode/BillModeCompositeKey.java
@@ -1,7 +1,10 @@
 package com.courier.overc360.api.idmaster.primary.model.billmode;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class BillModeCompositeKey implements Serializable {
 
     private static final long serialVersionUID = -7617672247680004647L;
